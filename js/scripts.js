93107d7df219ba1f130bd73d4d2bf52693265279
jacobpalaoro <jpalaoro197@gmail.com>
2022-06-17 12:14:26 -0700

    add ui to get an output of a new object through the select form and pizza constructor. WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-function Pizza(size, topping1, topping2, topping3, cheese) {
+function Pizza(size, cheese, topping1, topping2, topping3 ) {
   this.size = size;
+  this.cheese = cheese
   this.topping1 = topping1
   this.topping2 = topping2
   this.topping3 = topping3
-  this.cheese = cheese
 }
+
+
+$(document).ready(function() {
+  $("form#build-pizza").submit(function(event) {
+    event.preventDefault();
+    const selectedSize = $("select#size-select").val();
+    const selectedCheese = $("select#cheese-select").val();
+    const selectedTop1 = $("select#topping1-select").val();
+    const selectedTop2 = $("select#topping2-select").val();
+    const selectedTop3 = $("select#topping3-select").val();
+    let newPizza = new Pizza(selectedSize, selectedCheese, selectedTop1, selectedTop2, selectedTop3);
+    $("#your-pizza").html(newPizza);
+    console.log(newPizza);
+  })
+})
